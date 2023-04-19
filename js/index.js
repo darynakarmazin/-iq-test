f699b54cfb81c0809cf5c1789e5975b454aaadb7
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-04-19 22:23:03 +0300

    Add getElementById and addEventListener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,77 @@
+const DATA = [
+  {
+    question: "ваш пол:",
+    answers: [
+      {
+        id: "01",
+        value: "Мужчина",
+      },
+      {
+        id: "02",
+        value: "Женщина",
+      },
+    ],
+  },
+  {
+    question: "Укажите ваш возраст: ",
+    answers: [
+      {
+        id: "11",
+        value: "До 18",
+      },
+      {
+        id: "12",
+        value: "От 18 до 28",
+      },
+      {
+        id: "13",
+        value: "от 29 до 35",
+      },
+      {
+        id: "14",
+        value: "От 36",
+      },
+    ],
+  },
+  {
+    question: "Выберите лишнее:",
+    answers: [
+      {
+        id: "21",
+        value: "Дом",
+      },
+      {
+        id: "22",
+        value: "Шалаш",
+      },
+      {
+        id: "23",
+        value: "Бунгало",
+      },
+      {
+        id: "24",
+        value: "Скамейка",
+      },
+      {
+        id: "25",
+        value: "Хижина",
+      },
+    ],
+  },
+];
 
+const quiz = document.getElementById("quiz");
+const indicator = document.getElementById("indicator");
+const questions = document.getElementById("questions");
+const results = document.getElementById("results");
+const btnNext = document.getElementById("btn-next");
+
+const renderQuestions = () => {};
+
+const renderResults = () => {};
+
+const renderIndicator = () => {};
+
+quiz.addEventListener("change", (event) => { });
+
+btnNext.addEventListener("click", (event) => {});
