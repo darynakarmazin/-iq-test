56059dfb7d84d16c16bfd6ace4faed20b16fe7bd
Daryna Karmazin <daryna.karmazin@gmail.com>
2023-04-20 00:29:02 +0300

    Add function render resulds

--- js/index.js
+++ js/index.js
@@ -3,11 +3,11 @@ const DATA = [
     question: "ваш пол:",
     answers: [
       {
-        id: "01",
+        id: "0.1",
         value: "Мужчина",
       },
       {
-        id: "02",
+        id: "0.2",
         value: "Женщина",
       },
     ],
@@ -16,19 +16,19 @@ const DATA = [
     question: "Укажите ваш возраст: ",
     answers: [
       {
-        id: "11",
+        id: "1.1",
         value: "До 18",
       },
       {
-        id: "12",
+        id: "1.2",
         value: "От 18 до 28",
       },
       {
-        id: "13",
+        id: "1.3",
         value: "от 29 до 35",
       },
       {
-        id: "14",
+        id: "1.4",
         value: "От 36",
       },
     ],
@@ -37,28 +37,30 @@ const DATA = [
     question: "Выберите лишнее:",
     answers: [
       {
-        id: "21",
+        id: "2.1",
         value: "Дом",
       },
       {
-        id: "22",
+        id: "2.2",
         value: "Шалаш",
       },
       {
-        id: "23",
+        id: "2.3",
         value: "Бунгало",
       },
       {
-        id: "24",
+        id: "2.4",
         value: "Скамейка",
       },
       {
-        id: "25",
+        id: "2.5",
         value: "Хижина",
       },
     ],
   },
 ];
+
+let localResults = {};
 
 const quiz = document.getElementById("quiz");
 const indicator = document.getElementById("indicator");
@@ -66,12 +68,73 @@ const questions = document.getElementById("questions");
 const results = document.getElementById("results");
 const btnNext = document.getElementById("btn-next");
 
-const renderQuestions = () => {};
+const renderQuestions = (index) => {
+  renderIndicator(index + 1);
 
-const renderResults = () => {};
+  questions.dataset.currentStep = index;
 
-const renderIndicator = () => {};
+  const renderAnswers = () => {
+    return DATA[index].answers
+      .map((answer) => {
+        return `<li class="quiz-questions-str">
+            <label>
+                <input class="answer-input" type="radio" name=${index} value="${answer.id}"/>${answer.value}
+            </label>
+        </li>`;
+      })
+      .join("");
+  };
+  questions.innerHTML = `
+  <div class="quiz-questions-item">
+    <div class="quiz-questions-item__question">${DATA[index].question}</div>
+    <ul class="quiz-questions-item__answers">${renderAnswers()}</ul>
+  </div>`;
+};
 
-quiz.addEventListener("change", (event) => { });
+const renderResults = () => {
+  let content = "";
 
-btnNext.addEventListener("click", (event) => {});
+  const getAnswers = (index) => {
+    return DATA[index].answers
+      .map((answer) => {
+        return `<li class="quiz-results-str">${answer.value}</li>`;
+      })
+      .join("");
+  };
+
+  DATA.forEach((question, index) => {
+    content += `
+    <div class="quiz-results-item">
+        div class="quiz-results-item__question">${question.question}</div>
+        <ul class="quiz-results-item__answers">${getAnswers[index]}</ul>
+    </div>`;
+  });
+  results.innerHTML = content;
+};
+
+const renderIndicator = (currentStep) => {
+  indicator.innerHTML = `${currentStep}/${DATA.length}`;
+};
+
+quiz.addEventListener("change", (event) => {
+  if (event.target.classList.contains("answer-input")) {
+    localResults[event.target.name] = event.target.value;
+    btnNext.disabled = false;
+    console.log(localResults);
+  }
+});
+
+btnNext.addEventListener("click", (event) => {
+  if (DATA.length === Number(questions.dataset.currentStep) + 1) {
+    indicator.classList.add("indicator--hidden");
+    questions.classList.add("questions--hidden");
+    results.classList.add("results--visible");
+    btnNext.classList.add("btn-next--hidden");
+    renderResults();
+  } else {
+    renderQuestions(Number(questions.dataset.currentStep) + 1);
+  }
+  btnNext.disabled = true;
+});
+
+renderQuestions(0);
